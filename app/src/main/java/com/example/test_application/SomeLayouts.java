eb35c0c6d9aa799c01b6c5d5e14e5e139d0909ee
Ryosuke Shimizu <r.shimizu18020@gmail.com>
2019-11-11 00:44:38 +0900

    Add progressbar in SomeLayouts page

--- app/src/main/java/com/example/test_application/SomeLayouts.java
+++ app/src/main/java/com/example/test_application/SomeLayouts.java
@@ -2,12 +2,32 @@ package com.example.test_application;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class SomeLayouts extends AppCompatActivity {
+
+    private ProgressBar progressBar;
+    private int val = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_some_layouts);
+
+        progressBar = findViewById(R.id.progressbar);
+        progressBar.setMax(100);
+
+        Button button = findViewById(R.id.progressbar_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                val += 10;
+                progressBar.setProgress(val);
+                progressBar.setSecondaryProgress(50);
+            }
+        });
     }
+
 }
